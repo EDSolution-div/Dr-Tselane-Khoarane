38c3702e90c5f96fa8f4c39509865874099972b7
EDSolution-div <eliasdigitals.eds@gmail.com>
2025-05-20 03:55:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var menu = document.getElementById('menu');
 var nav = document.querySelector('#Navbar ul');
+let faq = document.querySelectorAll('.FAQ-container .Faq');
 const backTop = document.querySelector('.backTop');
 let counts = document.querySelectorAll(".count");
 let interval = 5000;
@@ -22,6 +23,22 @@ menu.onclick = function() {
     menu.classList.toggle('open-menu');
     nav.classList.toggle('view')
 };
+
+faq.forEach(Faq => {
+    
+    let heading = Faq.querySelector('.heading');
+    let icon = Faq.querySelector('.heading i');
+
+    heading.onclick = () => {
+        for(var i = 0; i < faq.length; i++){
+            if(faq[i] != Faq){
+                faq[i].classList.remove('active');
+            } else {
+                Faq.classList.toggle('active');
+            }
+        }
+    };
+});
 
 window.addEventListener('scroll', () => {
     if(window.pageYOffset > 100) {
